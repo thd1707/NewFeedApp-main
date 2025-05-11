999f1733153822f894238d3b863cc60c638f9eaf
Hong Duc <tranhongduchkn@gmail.com>
2025-05-11 10:42:34 +0700

    Initial Commit

---
+++ NewFeedApp/NewFeedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewFeedAppApp.swift
+//  NewFeedApp
+//
+//  Created by TY on 11/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct NewFeedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
